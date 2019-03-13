574124c32e63dd03629c16a5e3c75da4ecc82564
hummer010 <rob.mcculley@gmail.com>
2019-03-12 21:57:11 -0600

    CFixed type C

--- 00008-Enable-Open-Gapps.sh
+++ 00008-Enable-Open-Gapps.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-BUILD_DIR=/home/rob/ROS/.repo
+
 sed -i "s/# Audio/GAPPS_VARIANT := pico/" ${BUILD_DIR}/device/manufacturer/product/device.mk
 sed -i 's/#IMU calibration/\$(call inherit-product, vender\/opengapps\/build\/opengapps-packages.mk)/' ${BUILD_DIR}/device/manufacturer/product/device.mk
 
