cca91c7546e6168fc179b62552a9081142214626
unknown <unknown@localhost>
2019-03-15 20:31:15 +0000

    fixed path issues to modify device.mk

--- 00008-Enable-Open-Gapps.sh
+++ 00008-Enable-Open-Gapps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sed -i "s/# Audio/GAPPS_VARIANT := pico/" ${BUILD_DIR}/device/manufacturer/product/device.mk
-sed -i 's/#IMU calibration/\$(call inherit-product, vender\/opengapps\/build\/opengapps-packages.mk)/' ${BUILD_DIR}/device/manufacturer/product/device.mk
+sed -i "s/# Audio/GAPPS_VARIANT := pico/" ${BUILD_DIR}/device/google/taimen/device.mk
+sed -i 's/#IMU calibration/\$(call inherit-product, vender\/opengapps\/build\/opengapps-packages.mk)/' ${BUILD_DIR}/device/google/taimen/device.mk
 
 
